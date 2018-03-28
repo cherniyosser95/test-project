24567f9ed6c18b1b3fd7d7acd818667753aab9fd
cherniyosser <ch_yosser@outlook.sa>
2018-03-29 00:06:38 +0100

    user,role api

---
+++ app/Http/Controllers/JwtAuthenticateController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Permission;
+use App\Role;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Log;
+use App\Http\Requests\RoleRequest;
+use App\Http\Requests\PermissionRequest;
+class JwtAuthenticateController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['auth'=>Auth::user(), 'users'=>User::all()]);
+    }
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+      try {
+            // verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // if no errors are encountered we can return a JWT
+        return response()->json(compact('token'));
+    }
+        public function createRole(RoleRequest $request)
+        {
+            $role = new Role();
+            $role->name = $request->input('name');
+            if(($role->name == "admin")|| ($role->name == "user")) {
+                $role->save();
+            }
+            else{
+                "try to create valid role";
+            }
+            return response()->json("created");
+        }
+
+        public function createPermission(PermissionRequest $request){
+            $viewUsers = new Permission();
+            $viewUsers->name = $request->input('name');
+            $viewUsers->save();
+
+            return response()->json("created");
+        }
+        public function assignRole(Request $request){
+            $user = User::where('email', '=', $request->input('email'))->first();
+            $role = Role::where('name', '=', $request->input('role'))->first();
+            //$user->attachRole($request->input('role'));
+            $user->roles()->attach($role->id);
+
+            return response()->json("created");
+        }
+
+        public function attachPermission(Request $request){
+            $role = Role::where('name', '=', $request->input('role'))->first();
+            $permission = Permission::where('name', '=', $request->input('name'))->first();
+            $role->attachPermission($permission);
+            return response()->json("created");
+        }
+}
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route to create a new role
+Route::post('role', 'JwtAuthenticateController@createRole');
+// Route to create a new permission
+Route::post('permission', 'JwtAuthenticateController@createPermission');
+// Route to assign role to user
+Route::post('assign-role', 'JwtAuthenticateController@assignRole');
+// Route to attache permission to a role
+Route::post('attach-permission', 'JwtAuthenticateController@attachPermission');
+
+// API route group that we need to protect
+Route::group(['prefix' => 'api', 'middleware' => ['ability:admin,create-users']], function()
+{
+    // Protected route
+    Route::get('users', 'JwtAuthenticateController@index');
+});
+
+// Authentication route
+Route::post('authenticate', 'JwtAuthenticateController@authenticate');
+
+Route::group(['prefix' => 'api', 'middleware' => ['ability:admin,create-users']], function()
+{
+    Route::get('users', 'JwtAuthenticateController@index');
+
+});
+
+Route::group(['prefix' => 'api/v1', 'namespace' => 'Api'], function () {
+
+    Route::post('/auth/register', ['as' => 'auth.register', 'uses' => 'AuthController@register']);
+    Route::post('/auth/login', ['as' => 'auth.login', 'uses' => 'AuthController@login']);
+
+});
